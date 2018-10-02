236b83037208cd0d34468aa64af5ec4be2473d40
egorikas <egorgrishechko@gmail.com>
2018-10-02 22:01:27 +0300

    https://github.com/DotNetRu/Server/issues/4 playing with bot-api

---
+++ TelegramBotTheory/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading.Tasks;
+using MihaZupan;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+
+namespace TelegramBotTheory
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+
+            var proxy = new HttpToSocks5Proxy(
+                "", 0, "", ""
+            );
+            proxy.ResolveHostnamesLocally = true;
+            
+            var botClient = new TelegramBotClient("", proxy);
+            var s = await botClient.SendTextMessageAsync("@dot_net_ru_test_channel", "hey, this message from the bot");
+
+            var chat = await botClient.GetChatAsync(new ChatId("@dot_net_ru_test_channel"));
+        }
+    }
+}
